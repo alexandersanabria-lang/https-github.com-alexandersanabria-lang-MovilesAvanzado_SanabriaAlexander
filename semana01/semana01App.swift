dba8a62bc40c563eb323fdda7839e5ff3e1c4281
Alexander Sanabria <alexander.sanabria@tecsup.edu.pe>
2026-08-26 09:38:51 -0500

    Initial Commit

---
+++ semana01/semana01App.swift
@@ -0,0 +1,17 @@
+//
+//  semana01App.swift
+//  semana01
+//
+//  Created by Tecsup on 26/08/26.
+//
+
+import SwiftUI
+
+@main
+struct semana01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
